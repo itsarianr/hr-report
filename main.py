fc09ddb2ecb03f860e5ef372bc34b0afb86f3a53
Arian <itsarianr@gmail.com>
2018-11-03 12:07:30 +0330

    Fixed hour-minute problem. Refactored some pieces.

--- main.py
+++ main.py
@@ -5,48 +5,60 @@ from shamsi_date import Date
 
 str_start = '97-07-28'
 str_end = '97-08-06'
-CATEGORIES = {}
+CATEGORY_DURATIONS = {}
 
 
-def calculate_by_date(date):
-    directory = os.fsencode(date.get_directory_path())
-    if not os.path.isdir(directory):
+def read_durations_from_file_by_date(date):
+    reports_directory = os.fsencode(date.get_directory_path())
+    if not os.path.isdir(reports_directory):
         return
-    # print('==> ' + date.get_str())
-    for file in os.listdir(directory):
-        file_path = os.path.join(date.get_directory_path(), os.fsdecode(file))
-        sheet = pd.read_excel(file_path, sheet_name="Daily")
+    for report in os.listdir(reports_directory):
+        report_path = os.path.join(
+            date.get_directory_path(), os.fsdecode(report))
+        sheet = pd.read_excel(report_path, sheet_name="Daily")
         for index, category in enumerate(sheet.iloc[:, 2]):
             if type(category) is float:
                 break
-            time = sheet.iloc[:, 3][index]
-            if category not in CATEGORIES:
-                CATEGORIES[category] = 0
-            CATEGORIES[category] += time
+            if category not in CATEGORY_DURATIONS:
+                CATEGORY_DURATIONS[category] = 0
+            duration = sheet.iloc[:, 3][index]
+            CATEGORY_DURATIONS[category] += duration
 
 
-date = Date(str_start)
-end_date = Date(str_end)
-should_continue = True
-while should_continue:
-    if date.equals(end_date):
-        should_continue = False
-    calculate_by_date(date)
-    date = date.get_tommorow()
+def read_durations_in_date_range(str_start_date, str_end_date):
+    date = Date(str_start_date)
+    end_date = Date(str_end_date)
+    should_continue = True
+    while should_continue:
+        if date.equals(end_date):
+            should_continue = False
+        read_durations_from_file_by_date(date)
+        date = date.get_tommorow()
 
-sum = 0
-for category, time in CATEGORIES.items():
-    sum += time
-CATEGORY_PERCENTAGES = {}
-output = open(str_start + '~' + str_end + '.out', 'w', encoding='utf-8')
-for category, time in CATEGORIES.items():
-    CATEGORY_PERCENTAGES[category] = (time * 100 / sum).round()
-    str_category = category
-    if category == 'سایر':
-        str_category = 'Other'
-    if category == 'آموزش':
-        str_category = 'Education'
-    output.write(
-        str_category + ': ' + str(int(CATEGORY_PERCENTAGES[category])) + '% (' + str('{:,}'.format(int(CATEGORIES[category]))) + ' Hours)\n')
-output.write('=' * 30 + '\n')
-output.write('SUM: ' + str('{:,}'.format(int(sum))) + ' Hours')
+
+def generate_categories_report(str_start_date, str_end_date):
+    read_durations_in_date_range(str_start_date, str_end_date)
+    total_duration = 0
+    for category, duration in CATEGORY_DURATIONS.items():
+        total_duration += duration
+    category_percentages = {}
+    output_report = open(str_start_date + '~'
+                         + str_end_date + '.out', 'w', encoding='utf-8')
+    sorted_category_durations = sorted(
+        CATEGORY_DURATIONS.items(), key=lambda x: x[1], reverse=True)
+    for category_duration in sorted_category_durations:
+        category_percentages[category_duration[0]] = (
+            category_duration[1] * 100 / total_duration).round()
+        str_category = category_duration[0]
+        if category_duration[0] == 'سایر':
+            str_category = 'Other'
+        if category_duration[0] == 'آموزش':
+            str_category = 'Education'
+        output_report.write(
+            str_category + ': ' + str(int(category_percentages[category_duration[0]])) + '% (' + str(int(category_duration[1] / 60)) + ' Hours)\n')
+    output_report.write('=' * 25 + '\n')
+    output_report.write('SUM: ' + str(int(total_duration / 60)) + ' Hours')
+
+
+generate_categories_report(str_start, str_end)
+print('Done!')
